a2d43ad06cba8e365cfffa620c9b15502d254197
Jack <zhaoguangxu@192.168.31.64>
2025-02-12 10:48:17 +0800

    change: prompt_base

--- src/utils/deepseek.ts
+++ src/utils/deepseek.ts
@@ -12,40 +12,24 @@ export const generateCode = async (params: CodeGenerationParams) => {
   let data = JSON.stringify({
     messages: [
       {
-        content: 'You are a helpful assistant',
-        role: 'system',
-      },
-      {
-        content: 'Hi',
+        content: `生成${params.lang}组件代码，要求：${params.prompt}。使用最新语法，添加详细注释`,
         role: 'user',
       },
     ],
     model: 'deepseek-chat',
-    frequency_penalty: 0,
+
     max_tokens: 2048,
-    presence_penalty: 0,
-    response_format: {
-      type: 'text',
-    },
-    stop: null,
-    stream: false,
-    stream_options: null,
-    temperature: 1,
-    top_p: 1,
-    tools: null,
-    tool_choice: 'none',
-    logprobs: false,
-    top_logprobs: null,
+    temperature: 0.7,
   })
   try {
     const response = await axios.post(
       '/api',
       // 'https://api.deepseek.com/chat/completions',
-      // {
+      // JSON.stringify({
       //   prompt: `生成${params.lang}组件代码，要求：${params.prompt}。使用最新语法，添加详细注释`,
       //   temperature: 0.7,
       //   max_tokens: 1000,
-      // },
+      // }),
       data,
       {
         headers: {
@@ -55,7 +39,7 @@ export const generateCode = async (params: CodeGenerationParams) => {
         },
       },
     )
-    return response.data.choices[0].text
+    return response.data.choices[0].message.content
   } catch (error) {
     ElMessage.error('服务器请求繁忙，请稍后再试')
   }
